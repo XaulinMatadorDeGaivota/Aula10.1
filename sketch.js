773cef55121d2dddc528f676b6ef7f80f3dbc22e
Matheus Cabral Calil <93673317+XaulinMatadorDeGaivota@users.noreply.github.com>
2021-11-05 14:19:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var eusoulindo = [12,10,50,2,301];
+
+function setup() {
+  var soma = eusoulindo[0]+eusoulindo[1]+eusoulindo[2]+eusoulindo[3]+eusoulindo[4];
+  var notaescolar = soma / eusoulindo.length;
+  console.log(notaescolar);
+
+  createCanvas(400,400);
+}
+
+function draw() {
+background(51);
+
+}
+
